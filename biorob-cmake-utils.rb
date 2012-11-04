0da8e96c39d56f71d0cb9257dc49a46fab37aac3
Alexandre Tuleu <alexandre.tuleu.2005@polytechnique.org>
2012-11-04 23:52:42 +0100

    Fix commit targetting issue in BiorobCMakeUtils formula

--- biorob-cmake-utils.rb
+++ biorob-cmake-utils.rb
@@ -3,7 +3,7 @@ require 'formula'
 
 class BiorobCmakeUtils < Formula
   homepage 'https://ponyo.epfl.ch/redmine/projects/biorob-cmake-utils.cmake'
-  url 'https://ponyo.epfl.ch/git/biorob-cmake-utils.git', :using => :git, :commit => "1b6f2878f58d0450e54b405fd2569c1114f8d40b"
+  url 'https://ponyo.epfl.ch/git/biorob-cmake-utils.git', :using => :git, :revision => "1b6f2878f58d0450e54b405fd2569c1114f8d40b"
   version '0.3.2'
 
   depends_on 'cmake' => :build
