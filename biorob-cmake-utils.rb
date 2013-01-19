18cfc625265103f3ff35180d2a6853b0af42600a
Alexandre Tuleu <alexandre.tuleu.2005@polytechnique.org>
2013-01-19 16:49:55 +0100

    Upgrades biorob-cmake-utils to v0.3.4

--- biorob-cmake-utils.rb
+++ biorob-cmake-utils.rb
@@ -3,8 +3,8 @@ require 'formula'
 
 class BiorobCmakeUtils < Formula
   homepage 'https://ponyo.epfl.ch/redmine/projects/biorob-cmake-utils.cmake'
-  url 'https://ponyo.epfl.ch/git/biorob-cmake-utils.git', :using => :git, :revision => "b0fe50812b5fc0b1796d5d091c13a468626f4419"
-  version '0.3.3'
+  url 'https://ponyo.epfl.ch/git/biorob-cmake-utils.git', :using => :git, :revision => "a027d03ad1b308ad8d84121b7dcb2a5b0f5fadfb"
+  version '0.3.4'
 
   depends_on 'cmake' => :build
 
