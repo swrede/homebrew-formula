621431ab66f31dddfd5d9e87c3c7bde4082861c9
Alexandre Tuleu <alexandre.tuleu.2005@polytechnique.org>
2013-03-26 09:00:55 +0100

    Upgrades biorob-cpp to 0.3

--- biorob-cpp.rb
+++ biorob-cpp.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class BiorobCpp < Formula
-  homepage 'https://ponyo.epfl.ch/redmine/projects/biorob-cpp'
-  url 'https://ponyo.epfl.ch/git/biorob-cpp.git' , :using => :git, :revision => "b04c8d2d61d86008201e3b3e6cbf4d0c1d940e8d"
-  version '0.2.8'
+  homepage 'https://ponyo.epfl.ch/gitlab/core/projects/biorob-cpp'
+  url 'https://ponyo.epfl.ch/gitlab/core/biorob-cpp.git' , :using => :git, :revision => "3d8f1e75cd6f80d54af20ba5e316a9502356fe95"
+  version '0.3.0'
 
   depends_on 'cmake' => :build
   depends_on 'biorob-cmake-utils' => :build
