1cb9422ecbca50998638f89bd4b218cd63659a97
Sebastian Wrede <sebwrede@googlemail.com>
2013-04-24 20:26:52 +0200

    added URL to allow HEAD installation

--- biorob-cmake-utils.rb
+++ biorob-cmake-utils.rb
@@ -3,6 +3,7 @@ require 'formula'
 
 class BiorobCmakeUtils < Formula
   homepage 'https://ponyo.epfl.ch/redmine/projects/biorob-cmake-utils.cmake'
+  head 'https://ponyo.epfl.ch/git/biorob-cmake-utils.git', :using => :git
   url 'https://ponyo.epfl.ch/git/biorob-cmake-utils.git', :using => :git, :revision => "df730c643a6f91715685dbc7e2a2d465fc40888a"
   version '0.3.5'
 
